c523f01bc32a8670b08feb5dbc1fc6c593869281
Shahad <admin@Shahads-MacBook-Pro.local>
2025-09-20 17:59:10 +1000

    Initial commit - Assessment 3 blog app

--- resources/views/admin/posts/errors/404.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container text-center">
-    <h1 class="display-4">404</h1>
-    <p>Page not found.</p>
-    <a href="{{ url('/') }}" class="btn btn-primary">Go Home</a>
-</div>
-@endsection
